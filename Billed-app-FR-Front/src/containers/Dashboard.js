0d0227b79fb9799cb684ffa888c012061dbf13ba
Enzo Ghisoni <enzo.ghisoni@hotmail.fr>
2022-08-20 08:19:43 +0200

    Fix the isssue in the dashboard

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -86,8 +86,8 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    //console.log(`this.counter: ${this.counter} this.id: ${this.id}`)
-    //console.log(`bill.id: ${bill.id}`)
+    console.log(`this.counter: ${this.counter} this.id: ${this.id}`)
+    console.log(`bill.id: ${bill.id}`)
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
@@ -136,19 +136,24 @@ export default class {
 
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
+    if (this.index === undefined || this.index !== index) {
+      this.index = index
+      this.counter ++
+    }
     if (this.counter % 2 === 0) {
       
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
       this.counter ++
+      
     } else {
       
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
         .html("")
       this.counter ++
+
     }
 
     bills.forEach(bill => {
